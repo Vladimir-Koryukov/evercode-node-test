50314e8eb4b5709c5f844d7a93f2821601af129d
Vladimir Koryukov <vladimirkorukov251@gmail.com>
2026-05-28 14:35:03 +0300

    fix structured logger for 1 module

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,10 +1,24 @@
 function createLogger(appName) {
-  function log(level, message, options = {}) {
+  const writers = {
+    error: console.error.bind(console),
+    warn: console.warn.bind(console),
+    info: console.info.bind(console),
+    debug: console.debug.bind(console),
+    trace: console.trace.bind(console),
+  };
+
+  function formatMessage(level, message, options = {}) {
     const timestamp = new Date().toISOString();
     const requestId = options.requestId ? `requestId = ${options.requestId}` : "";
 
-    console.log(`[${timestamp}] [${level.toUpperCase()}] [${appName}] ${requestId} ${message}`);
-  };
+    return `[${timestamp}] [${level.toUpperCase()}] [${appName}] ${requestId} ${message}`;
+  }
+
+  function log(level, message, options = {}) {
+    const writer = writers[level] || console.log.bind(console);
+
+    writer(formatMessage(level, message, options));
+  }
 
   return {
     error(message, options) {
