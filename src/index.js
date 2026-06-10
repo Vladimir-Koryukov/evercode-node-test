fc4beb5a79d96db8a5119cd7d7c4bd6e94d22f9b
Vladimir Koryukov <vladimirkorukov251@gmail.com>
2026-06-10 11:50:34 +0300

    add graceful shutdown

--- src/index.js
+++ src/index.js
@@ -6,4 +6,23 @@ const logger = createLogger(config.appName);
 
 logger.info('Price synchronization worker started');
 
-startPriceSyncTask(logger);
+const priceSyncInterval = startPriceSyncTask(logger);
+
+let isShuttingDown = false;
+
+function shutdown(signal) {
+  if (isShuttingDown) {
+    return;
+  }
+
+  isShuttingDown = true;
+
+  logger.info(`${signal} received. Stopping price synchronization worker`);
+
+  clearInterval(priceSyncInterval);
+
+  logger.info('Price synchronization worker stopped');
+}
+
+process.once('SIGINT', () => shutdown('SIGINT'));
+process.once('SIGTERM', () => shutdown('SIGTERM'));
